9e8374702bee1a7a1392d137773ec80e0c943b9f
borhan98 <borhanuddindns420@gmail.com>
2023-11-19 19:42:32 +0600

    get a single job by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -39,6 +39,13 @@ async function run() {
                 result = [];
                 result = await jobCollection.find().toArray();
             }
+            res.send(result);
+        })
+        
+        app.get("/jobs/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await jobCollection.findOne(query);
             res.send(result);
         })
 
