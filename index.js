12366f1a3f7798cf55d0f7057b23053aaddbc5ec
borhan98 <borhanuddindns420@gmail.com>
2023-11-22 14:48:58 +0600

    get applied jobs ids & jobs for user APIs created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
@@ -13,7 +14,7 @@ console.log(process.env.DB_USER, process.env.DB_PASS);
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://borhanuddindns420:j3Ds2nNCbxqSgznf@cluster0.dumb7x3.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dumb7x3.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -54,6 +55,14 @@ async function run() {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await jobCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.post("/jobsByIds", async (req, res) => {
+            const ids = req.body;
+            const objectIds = ids.map(id => new ObjectId(id));
+            const query = {_id: { $in: objectIds }};
+            const result = await jobCollection.find(query).toArray();
             res.send(result);
         })
 
@@ -105,6 +114,19 @@ async function run() {
         /*------------------------------------------------
                         applications related APIs 
         --------------------------------------------------*/
+        app.get("/applications", async (req, res) => {
+            let query = {};
+            let result = [];
+            if (req.query.email) {
+                const options  = {
+                    projection: { jobId: 1 }
+                }
+                query = {email: req.query.email}
+                result = await applicationCollection.find(query, options).toArray();
+            }
+            res.send(result);            
+        })
+
         app.get("/applications/:jobId", async (req, res) => {
             const jobId = req.params.jobId;
             const query = {jobId};
